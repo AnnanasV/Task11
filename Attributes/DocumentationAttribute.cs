ef100d8f253577aae7a4e867e64be5bd9c2c5f41
Анна <vindanka@gmail.com>
2024-10-25 14:06:00 +0300

    create attribute class and add to methods

---
+++ Attributes/DocumentationAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace Attributes
+{
+    public class DocumentationAttribute : Attribute
+    {
+        public string Author { get; set; }
+        public int Version { get; set; }
+
+        public DocumentationAttribute (string author)
+        {
+            Author = author;
+        }
+    }
+}
--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using Attributes;
+
+[Documentation("Anna", Version = 1)]
+void Method1 () { }
+
+[Documentation("Pavel", Version = 2)]
+void Method2 () { }
+
+[Documentation("Liza", Version = 1)]
+void Method3 () { }
